875191bc4699aa5a7972d0361cec35e91651cf54
ProgramerXYZ <194214968+ProgramerXYZ@users.noreply.github.com>
2025-09-18 15:53:06 +0530

    restart

--- main.cpp
+++ main.cpp
@@ -5,17 +5,8 @@ using namespace std;
 
 int main()
 {
-    srand(time(0)); // seed with current time
-    
-    int input;
-
-    cout<<"welcome to randoum number gusser"<<endl;
-    cout<<"plz select your difficulty 1 for easy \n2 for medium \n3 for hard ";
-    cin>>input;
-    if (input==1)
-    {
-        auto randam = rand(10);
-    }
+   
+  
     
 
     return 0;
